f109c94efa805eeb059de965c027066077deb3fb
metalculus84 <adkison.adrian@gmail.com>
2010-07-28 09:04:52 -0700

    I introduced a small section for ie6 because the browser fires the window resize event whenever doing any DOM manipulation. This stops infinite loops.

--- dialog/jquery.ui.dialog.autoReposition.js
+++ dialog/jquery.ui.dialog.autoReposition.js
@@ -1,13 +1,23 @@
 (function( $ ) {
-
+var prevHeight;
 $.ui.dialog.prototype.options.autoReposition = true;
 $( window ).resize(function() {
+    
+    /* This really only needs to be here for ie6 because the browser fires the window resize event whenever doing any     DOM manipulation.  Infinite loops ensue*/
+    var height = jQuery(window).height();
+    if (prevHeight == height) {
+        return;
+    } else {
+        prevHeight = height;
+    }
+
     $( ".ui-dialog-content:visible" ).each(function() {
         var dialog = $( this ).data( "dialog" );
         if ( dialog.options.autoReposition ) {
             dialog.option( "position", dialog.options.position );
         }
     });
+
 });
 
 })( jQuery );
